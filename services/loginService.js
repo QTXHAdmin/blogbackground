a9b194c52588eaf8d6def780df7ec5cf4e1870b9
zhao5840980 <2439421565@qq.com>
2019-03-07 21:04:58 +0800

    完成了用户登录后台验证、添加用户

--- services/loginService.js
+++ services/loginService.js
@@ -7,13 +7,56 @@ const loginService = {
     //保存数据进mongoDB
     newAccount.save((err, data) => {
       if (err) {
-        return err;
+        // return err;
+        data = {
+          code: 1,
+          msg: '添加用户失败',
+          err: err
+        }
       } else {
-        console.log('保存成功！');
-        // return data = 'createUser successed' + data;
-        callback(data);
+        data = {
+          code: 0,
+          mag: '添加用户成功',
+          data: data
+        }
+
       }
+      callback(data);
     });
+
+  },
+  validateUser: (user, callback) => {
+
+    models.User.find({
+        userName: user.userName
+      }, function (err, doc) {
+        if (err) {
+          return
+        };
+        console.log(doc);
+        if (!doc.length) {
+          data = {
+            code: 1,
+            msg: '用户名错误'
+          }
+        } else {
+          if (user.pwd === doc[0].pwd) {
+            data = {
+              code: 0,
+              msg: '登录成功!'
+            }
+          } else {
+            data = {
+              code: 1,
+              msg: '密码错误!'
+            }
+          }
+        }
+        callback(data);
+
+      }
+
+    );
 
   }
 
